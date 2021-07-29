e407d6d69a1877c988de8acf3e3079945184fb39
xuannga <nguyenx206@gmail.com>
2021-07-28 17:05:17 -0700

    added prompts

--- db/schema.sql
+++ db/schema.sql
@@ -12,7 +12,9 @@ CREATE TABLE role (
     id INT NOT NULL PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT NOT NULL
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
 );
 
 CREATE TABLE employee (
@@ -20,6 +22,10 @@ CREATE TABLE employee (
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT NOT NULL
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id),
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
 );
 
--- server.js
+++ server.js
@@ -1,13 +1,144 @@
-const express = require('express');
+const inquirer = require('inquirer');
 // Import and require mysql2
 const mysql = require('mysql2');
-
 const cTable = require('console.table');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+function initApp() {
+    runPrompts();
+}
 
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+function runPrompts(){
+    prompt([
+        {
+            //prompts to load when npm start
+            type: "list",
+            name: "options",
+            message: "What do you want to to?",
+            choices:[
+                {
+                    name: "View ALL Departments",
+                    value: "VIEW_DEPARTMENTS"
+                },
+                {
+                    name: "View ALL Roles",
+                    value: "VIEW_ROLES"
+                },
+                {
+                    name: "View ALL Employees",
+                    value: "VIEW_EMPLOYEES"
+                },
+                {
+                    name: "Add a Department",
+                    value: "ADD_DEPARTMENT"
+                },
+                {
+                    name: "Add a Role",
+                    value: "ADD_ROLE"
+                },
+                {
+                    name: "Add an Employee",
+                    value: "ADD_EMPLOYEE"
+                },
+                {
+                    name: "Update Employee Role",
+                    value: "UPDATE_EMPLOYEE_ROLE"
+                },
+                {
+                    name: "Quit",
+                    value: "QUITE"
+                }
+            ]
+        }
+    ]).then(response =>{
+        let choice = response.options;
+        switch(choice) {
+            case "VIEW_DEPARTMENTS":
+                viewAllDepart();
+                break;
+            case "VIEW_ROLES":
+                viewAllRoles();
+                break;
+            case "VIEW_EMPLOYEES":
+                viewAllEmployees();
+                break;
+            case "ADD_DEPARTMENT":
+                createDepartment();
+                break;
+            case "ADD_ROLE":
+                createRole();
+                break;
+            case "ADD_EMPLOYEE":
+                createEmployee();
+                break;
+            case "UPDATE_EMPLOYEE_ROLE":
+                updateEmployeeRole();
+                break;
+                default:
+                    quit();
+        }
+    })
+}
 
+//view all employee
+function viewAllEmployees() {
+    db.allEmployees()
+        .then(([rows]) => {
+            let employees = rows;
+            console.log("\n");
+            console.table(employees);
+        }).then(()=> runPrompts());
+}
+
+//view all roles
+function viewAllRoles() {
+    db.allRoles()
+        .then(([rows]) => {
+            let roles = rows;
+            console.log("\n");
+            console.table(roles);
+        }).then(()=> runPrompts());
+}
+
+//view all departments
+function viewAllDepart() {
+    db.allDepart()
+        .then(([rows]) => {
+            let depart = rows;
+            console.log("\n");
+            console.table(depart);
+        }).then(()=> runPrompts());
+}
+
+//add a role
+function createRole(){
+    db.allDepart()
+        .then(([rows]) => {
+            let departments = rows;
+            const departChoices = rows;
+            const departChoices = departments.map(({id, name}) => ({
+                name: name,
+                value: id
+            }));
+
+            prompt([
+                {
+                    name: "title",
+                    message: "What is the role name?"
+                },
+                {
+                    name: "salary",
+                    message: "What is the salary?"
+                },
+                {
+                    type: "list",
+                    name: "department_id",
+                    message: "Which department does the role belong to?",
+                    choices: departChoices
+                }
+            ]).then (role => {
+                db.addRole(role)
+                    .then(() => console.log(`Added ${role.title} to the databse`))
+                    .then(() => runPrompts())
+            })
+        })
+}
